c8dea5949a3e494fe7e7f9a73013d2fb4718dd77
Pratik Marathe <86453383+PratikVMarathe@users.noreply.github.com>
2024-04-17 17:16:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,15 +5,12 @@ const categoriesRouter = require('./routes/categories');
 const productsRouter = require('./routes/products');
 const app = express();
 
-// Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
-// Routes
 app.use('/api/categories', categoriesRouter);
 app.use('/api/products', productsRouter);
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
