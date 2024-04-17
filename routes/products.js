6fdad9b02d4e30eca4316b653ddc18cbaf9522a7
Pratik Marathe <86453383+PratikVMarathe@users.noreply.github.com>
2024-04-17 17:15:56 +0530

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -6,7 +6,6 @@ router.post('/', ProductController.createProduct);
 router.get('/', ProductController.getAllProducts);
 router.put('/:id', ProductController.updateProduct);
 router.delete('/:id', ProductController.deleteProduct);
-// Associate a product with a category
 router.put('/:id/categories/:categoryId', ProductController.associateProductCategory);
 
 module.exports = router;
